62ebc98dcda5fb18daac4c1148a39ef0ca63093a
JackHuynh2001 <9jhuynh@stpatrick.org>
2016-12-13 13:54:17 -0600

    Add files via upload

--- First-Semester-Final-master/cart.js
+++ First-Semester-Final-master/cart.js
@@ -1,10 +1,24 @@
 class Cart{
   //What is the first part of every class? Type it below.
-constructor(iL, iQ){
-  this.itemList=iL;
-  this.itemQuantity=iQ;
+constructor(itemList, itemQuantity){
+  this.itemList=itemList;
+  this.itemQuantity=itemQuanitity;
 }
   //Type the instance functions below this comment.
 additem(i,q){
-  
+  this.item=push(i);
+  this.quantity=push(q);
+}
+totalCart(){
+  var total=t;
+  for(item=i;i<itemlist.length,i++){
+    total=
+
+
+  }
+
+}
+
+
+
 }
--- First-Semester-Final-master/item.js
+++ First-Semester-Final-master/item.js
@@ -1,6 +1,15 @@
 class Item{
-  //Fill in the item class below this comment.
+  constructor(n,p,s){
+    this.name=n;
+    this.price=p;
+    this.shipping=s;
 
+
+  }
+  //Fill in the item class below this comment.
+this.item=new Item("Call Of Duty 20",60,3)
+this.item=new Item("Macbook Pro Future Edition",2000,14)
+this.item=new Iteam("Expenisve GPU",500,7)
 
 
 }
